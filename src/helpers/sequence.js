bb1746485f1882db16cdb8969ea7e8dc5ca482f5
Nicolas Corral <nico.corral@gmail.com>
2017-06-27 15:05:35 -0400

    feat: added gaussian sequence

--- src/helpers/sequence.js
+++ src/helpers/sequence.js
@@ -12,30 +12,41 @@ export const generateDotSequence = (length, rng) => {
 }
 
 export const generateSequence = (length, rng) => {
-    const numbers = [];
+    const numbers = []
 
     for (let counter = length; counter > 0; counter--) {
-        numbers.push(rng.generate());
+        numbers.push(rng.generate())
     }
 
-    return numbers;
+    return numbers
 }
 
 export const generateNormalSequence = (length, rng) => {
-    const numbers = [];
-    let x, y, r;
+    const numbers = []
+    let x, y, r
 
     for (let counter = length; counter > 0; counter--) {
         do {
-            x = 2 * rng.generate() - 1;
-            y = 2 * rng.generate() - 1;
-            r = x * x + y * y;
-        } while (r >= 1 || r === 0);
+            x = 2 * rng.generate() - 1
+            y = 2 * rng.generate() - 1
+            r = x * x + y * y
+        } while (r >= 1 || r === 0)
 
         numbers.push(
             x * Math.sqrt(-2 * Math.log(r) / r)
-        );
+        )
     }
 
-    return numbers;
+    return numbers
+}
+
+export const generateNormalDotSequence = (length, rng) => {
+    const sequence = generateNormalSequence(length * 2, rng)
+    return sequence.reduce((result, value, index) => {
+        if (index % 2 === 0) {
+            const dots = sequence.slice(index, index + 2)
+            result.push({ x: dots[0], y: dots[1]})
+        }
+        return result
+    }, [])
 }
